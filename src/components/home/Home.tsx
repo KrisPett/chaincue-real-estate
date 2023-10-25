9a26d832d22e54f05a33ec84f797d28b38ec5122
KQT3 <70701718+KQT3@users.noreply.github.com>
2023-10-25 20:37:58 +0200

    commit

--- src/components/home/Home.tsx
+++ src/components/home/Home.tsx
@@ -1,6 +1,7 @@
 "use client"
 import React, {useState} from 'react';
 import {FilterSearchReqBody, HouseTypes} from "@/components/home/HomeViewModel";
+import Button2 from "@/lib/Button2";
 
 const awsImagesLinks = [
   {name: "https://images.chaincuet.com/logos/ancient-rome.jpeg"},
@@ -45,88 +46,75 @@ const FilterButton = (props: FilterButtonProps) => {
   )
 }
 
-
 const Home = () => {
-      const [text, setText] = useState("");
-      const [textLines] = useState(1);
-      // const [isSelected, setIsSelected] = useState(false);
-      const [filterSearchContext, setFilterSearchContext] = useState<FilterSearchReqBody>({
-        country: "ANY",
-        textAreaSearchValue: "",
-        houseTypes: []
-      });
+  const [text, setText] = useState("");
+  const [textLines] = useState(1);
+  const [filterSearchContext, setFilterSearchContext] = useState<FilterSearchReqBody>({
+    country: "ANY",
+    textAreaSearchValue: "",
+    houseTypes: []
+  });
 
-      const handleTextareaOnKeydown = (event: React.KeyboardEvent<HTMLTextAreaElement>) => {
-        const isEnterPressed = event.key === "Enter";
-        if (isEnterPressed) {
-          setFilterSearchContext(prevState => ({
-            ...prevState,
-            textAreaSearchValue: text
-          }));
-        }
-        if (isEnterPressed) {
-          event.preventDefault();
-        }
-      }
+  const handleTextareaOnKeydown = (event: React.KeyboardEvent<HTMLTextAreaElement>) => {
+    const isEnterPressed = event.key === "Enter";
+    if (isEnterPressed) {
+      setFilterSearchContext(prevState => ({
+        ...prevState,
+        textAreaSearchValue: text
+      }));
+    }
+    if (isEnterPressed) {
+      event.preventDefault();
+    }
+  }
 
-      const handleTextareaOnBlur = () => {
-        setFilterSearchContext(prevState => ({
-          ...prevState,
-          textAreaSearchValue: text
-        }));
-      }
+  const handleTextareaOnBlur = () => {
+    setFilterSearchContext(prevState => ({
+      ...prevState,
+      textAreaSearchValue: text
+    }));
+  }
 
-      const onChangeOption = (event: React.ChangeEvent<HTMLSelectElement>) => {
-        setFilterSearchContext(prevState => ({
-          ...prevState,
-          country: event.target.value
-        }));
-      };
+  const onChangeOption = (event: React.ChangeEvent<HTMLSelectElement>) => {
+    setFilterSearchContext(prevState => ({
+      ...prevState,
+      country: event.target.value
+    }));
+  };
 
-      // const onClickAddHouseTypesIfNotExistsOrRemove = (houseTypes: HouseTypes) => {
-      //   setIsSelected(!isSelected);
-      //   setFilterSearchContext((prevState) => {
-      //     const houseTypeString = houseTypes.toString();
-      //     const updatedHouseTypes = prevState.houseTypes.includes(houseTypeString)
-      //         ? prevState.houseTypes.filter((type) => type !== houseTypeString)
-      //         : [...prevState.houseTypes, houseTypeString];
-      //     return {
-      //       ...prevState,
-      //       houseTypes: updatedHouseTypes,
-      //     };
-      //   });
-      // };
+  const onBtnSearch = () => {
+    console.log("onBtnSearch");
+    console.log(filterSearchContext);
+  };
 
-      console.log(filterSearchContext)
+  return (
+      <main className={"flex"}>
+        <div className={"w-full"}>
+          <div className={"h-full"}>
+            <div className="flex flex-col items-center bg-local h-full"
+                 style={{
+                   backgroundImage: `url(${awsImagesLinks[2].name})`,
+                   backgroundSize: "cover",
+                   backgroundRepeat: "no-repeat",
+                 }}>
+              <div className="flex justify-center border-amber-600 xxs:w-full md:w-10/12 xl:w-8/12 2xl:w-6/12 rounded h-full mb-5 mt-5 bg-zinc-50
+                                  opacity-95">
+                <div className={"flex flex-col gap-4 w-11/12 mt-8"}>
 
-      return (
-          <main className={"flex"}>
-            <div className={"w-full"}>
-              <div className={"h-96"}>
-                <div className="flex flex-col items-center bg-local h-full"
-                     style={{
-                       backgroundImage: `url(${awsImagesLinks[2].name})`,
-                       backgroundSize: "cover",
-                       backgroundRepeat: "no-repeat",
-                     }}>
-                  <div className="flex justify-center border-amber-600 xxs:w-full md:w-10/12 xl:w-8/12 2xl:w-6/12 rounded h-full mb-5 mt-5 bg-zinc-50
-                                  opacity-95" >
-                    <div className={"flex flex-col w-11/12 mt-1"}>
+                  <section aria-label={"select-country"} className={"md:w-3/12"}>
+                    <select id="countries"
+                            className="bg-gray-50 border border-amber-600 text-gray-900 text-sm rounded focus:ring-amber-600 focus:border-amber-600 block w-full p-2.5 hover:border-amber-400"
+                            defaultValue={"ANY"}
+                            onChange={event => onChangeOption(event)}
+                    >
+                      <option value={"ANY"}>Any Country</option>
+                      <option value="SWEDEN">Sweden</option>
+                      <option value="SPAIN">Spain</option>
+                    </select>
+                  </section>
 
-                      <section aria-label={"select-country"} className={"md:w-3/12"}>
-                        <select id="countries"
-                                className="bg-gray-50 border border-amber-600 text-gray-900 text-sm rounded focus:ring-amber-600 focus:border-amber-600 block w-full p-2.5 hover:border-amber-400"
-                                defaultValue={"ANY"}
-                                onChange={event => onChangeOption(event)}
-                        >
-                          <option value={"ANY"}>Any Country</option>
-                          <option value="SWEDEN">Sweden</option>
-                          <option value="SPAIN">Spain</option>
-                        </select>
-                      </section>
-
-                      <section aria-label={"textfield"} className={"flex flex-col items-center mt-2"}>
-                        <form className={"w-full"} onSubmit={(e) => e.preventDefault()}>
+                  <section aria-label={"textfield"} className={"flex flex-col items-center"}>
+                    <form className={"w-full"} onSubmit={(e) => e.preventDefault()}>
                     <textarea
                         className="textarea-bordered textarea w-full resize-none rounded bg-zinc-50 border-amber-600 bg-opacity-60 hover:border-amber-400
                         focus:ring-1 focus:ring-offset-2 focus:ring-offset-amber-600 focus-visible:border-0 focus:outline-none
@@ -135,35 +123,48 @@ const Home = () => {
                         onKeyDown={handleTextareaOnKeydown} value={text}
                         onBlur={event => handleTextareaOnBlur()}
                     />
-                        </form>
-                      </section>
+                    </form>
+                  </section>
 
-                      <section className={"grid md:grid-cols-4 lg:grid-cols-7 gap-4"}>
-                        <FilterButton title={"Condominium"} houseTypes={HouseTypes.CONDOMINIUM}
-                                      setFilterSearchContext={setFilterSearchContext}/>
-                        <FilterButton title={"Villa"} houseTypes={HouseTypes.VILLA}
-                                      setFilterSearchContext={setFilterSearchContext}/>
-                        <FilterButton title={"TownHouse"} houseTypes={HouseTypes.TOWNHOUSE}
-                                      setFilterSearchContext={setFilterSearchContext}/>
-                        <FilterButton title={"Vacation home"} houseTypes={HouseTypes.VACATION_HOME}
-                                      setFilterSearchContext={setFilterSearchContext}/>
-                        <FilterButton title={"Estates and Farms"} houseTypes={HouseTypes.ESTATES_AND_FARMS}
-                                      setFilterSearchContext={setFilterSearchContext}/>
-                        <FilterButton title={"Land"} houseTypes={HouseTypes.LAND}
-                                      setFilterSearchContext={setFilterSearchContext}/>
-                        <FilterButton title={"Other Houses"} houseTypes={HouseTypes.OTHER_HOUSES}
-                                      setFilterSearchContext={setFilterSearchContext}/>
-                      </section>
+                  <section aria-label={"filter_btn"} className={"grid md:grid-cols-4 lg:grid-cols-7 gap-4"}>
+                    <FilterButton title={"Condominium"} houseTypes={HouseTypes.CONDOMINIUM}
+                                  setFilterSearchContext={setFilterSearchContext}/>
+                    <FilterButton title={"Villa"} houseTypes={HouseTypes.VILLA}
+                                  setFilterSearchContext={setFilterSearchContext}/>
+                    <FilterButton title={"TownHouse"} houseTypes={HouseTypes.TOWNHOUSE}
+                                  setFilterSearchContext={setFilterSearchContext}/>
+                    <FilterButton title={"Vacation home"} houseTypes={HouseTypes.VACATION_HOME}
+                                  setFilterSearchContext={setFilterSearchContext}/>
+                    <FilterButton title={"Estates and Farms"} houseTypes={HouseTypes.ESTATES_AND_FARMS}
+                                  setFilterSearchContext={setFilterSearchContext}/>
+                    <FilterButton title={"Land"} houseTypes={HouseTypes.LAND}
+                                  setFilterSearchContext={setFilterSearchContext}/>
+                    <FilterButton title={"Other Houses"} houseTypes={HouseTypes.OTHER_HOUSES}
+                                  setFilterSearchContext={setFilterSearchContext}/>
+                  </section>
 
+                  <section aria-label={"divider"} className={"mt-8"}>
+                    {/*<div className="border-2 border-amber-600"></div>*/}
 
+                    <div className="flex items-center justify-center w-full border-amber-600">
+                      <div className="w-4 h-4 border-t-2 border-l-2 transform -rotate-45 border-amber-600"></div>
+                      <div className="border-2 w-full border-amber-600"></div>
+                      <div className="w-4 h-4 border-t-2 border-r-2 transform rotate-45 border-amber-600"></div>
                     </div>
-                  </div>
+
+
+                  </section>
+
+                  <section className={"mt-8 mb-8"}>
+                    <Button2 title={"Search"} onClick={() => onBtnSearch()}/>
+                  </section>
                 </div>
               </div>
             </div>
-          </main>
-      );
-    }
-;
+          </div>
+        </div>
+      </main>
+  )
+}
 
-export default Home;
+export default Home
---
+++ src/lib/Button2.tsx
@@ -0,0 +1,20 @@
+import React from "react";
+
+interface IButton {
+  title: string;
+  onClick: () => void;
+}
+
+const Button2 = ({title, onClick}: IButton) => {
+  return (
+      <button
+          onClick={onClick}
+          onDragEnd={onClick}
+          className="group btn relative rounded-md p-1 text-sm font-medium text-gray-800 hover:text-gray-700
+                                       hover:border-amber-500 bg-gradient-to-b from-gray-50 to-gray-50 xxs:w-full normal-case border-amber-600">
+        <span className="relative px-5 text-lg text-amber-700 whitespace-nowrap">{title}</span>
+      </button>
+  );
+};
+
+export default Button2;
